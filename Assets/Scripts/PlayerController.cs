0fc8164a387a26256a1040d58e58d9deb9267f37
Brandon Eduardo Retana García <142031627+beRetana@users.noreply.github.com>
2024-04-17 21:36:56 -0700

    Set up Cinemachine with movement
    - Set up Cinemachine virtual camera for first-person POV.
    - Created a player game object with a body and head, and created a plane.
    - Updated the player controller script to sync the direction of the camera with the player game object.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,8 @@
 //Coder: Brandon Retana
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+//Will not run unless a CharacterController component is added to the gameObject.
+[RequireComponent(typeof(CharacterController))]
 
 //Class is in charge of making the player move, jump and rotate.
 public class PlayerController : MonoBehaviour
@@ -12,6 +13,7 @@ public class PlayerController : MonoBehaviour
 
     private InputManager inputManager;
     private CharacterController controller;
+    private Transform cameraTransform;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
 
@@ -20,6 +22,7 @@ public class PlayerController : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         inputManager = InputManager.Instance;
+        cameraTransform = Camera.main.transform;
     }
 
     //Updates the player's position and jumping conditions getting the input of the user
@@ -32,13 +35,12 @@ public class PlayerController : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = (Vector3)inputManager.GetPlayerMovement();
+        Vector2 movement = inputManager.GetPlayerMovement();
+        Vector3 move = new Vector3(movement.x, 0f, movement.y);
+        move = cameraTransform.forward * move.z + cameraTransform.right * move.x;
+        move.y = 0f;
+        move.Normalize();
         controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
 
         // Changes the height position of the player..
         if (inputManager.PlayerJumpedThisFrame() && groundedPlayer)
