660d64ee8f50f5ec42bfbe40177805536ad9f20a
coleyoung02 <coleyoung741@gmail.com>
2024-04-30 18:28:41 -0700

    Show Laser on reflective bullet loaded

--- Assets/_Project/Scripts/Revolver/BulletScripts/Reflective.cs
+++ Assets/_Project/Scripts/Revolver/BulletScripts/Reflective.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Reflective : BulletBase
 {
     private RaycastHit nextWallHit;
+    private bool hitSet = false;
 
     void Start()
     {
@@ -19,26 +21,41 @@ public class Reflective : BulletBase
         {
             nextWallHit = hit;
         }
+        hitSet = didHit;
     }
 
-    public static Vector3[] GetPoints(Vector3 startPos, Vector3 dir)
+    public static List<Vector3> GetPoints(Vector3 startPos, Vector3 dir)
     {
         List<Vector3> points = new List<Vector3>();
         Vector3 hitPos = startPos;
-        for (int i = 0; i < 50; i++)
+        Vector3 outHitDir = dir;
+        for (int i = 0; i < 100; i++)
         {
             RaycastHit hit;
-            bool didHit = Physics.Raycast(hitPos, dir, out hit, Mathf.Infinity, LayerMask.GetMask("Reflective"), QueryTriggerInteraction.Collide);
+            bool didHit = Physics.Raycast(hitPos, outHitDir, out hit, Mathf.Infinity, LayerMask.GetMask("Reflective"), QueryTriggerInteraction.Collide);
             if (didHit)
             {
                 hitPos = hit.point;
+                outHitDir = ReflectOnPlane(outHitDir, hit.normal, hit.transform.up);
+                points.Add(hitPos);
             }
             else
             {
+                Debug.DrawRay(hitPos, outHitDir * 5, Color.green);
+                didHit = Physics.Raycast(hitPos, outHitDir, out hit, Mathf.Infinity);
+                if (didHit)
+                {
+                    points.Add(hit.point);
+                }
+                else
+                {
+                    points.Add(hitPos + outHitDir * 20f);
+                }
                 break;
             }
         }
-        return points.ToArray();
+        Debug.Log(points.Count);
+        return points;
     }
 
 
@@ -52,6 +69,11 @@ public class Reflective : BulletBase
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Reflective"))
         {
+            if (!hitSet)
+            {
+                Destroy(gameObject);
+                return;
+            }
             rb.velocity = ReflectOnPlane(rb.velocity, nextWallHit.normal, nextWallHit.transform.up);
             SetDirection(rb.velocity);
         }
