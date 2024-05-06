cef8b47cf5241dd1fe50a7584059107959f483c3
big-jungus <matthewjung1010@gmail.com>
2024-05-05 18:48:34 -0700

    bullet dragging

--- Assets/_Project/Scripts/Inventory/ChamberSlotView.cs
+++ Assets/_Project/Scripts/Inventory/ChamberSlotView.cs
@@ -2,23 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ChamberSlotView : MonoBehaviour
+
+public class ChamberSlotView : MonoBehaviour, IDropHandler
 {
 	[SerializeField] private int _slotIndex;
 	
 	private BulletObject _bulletToDisplay;
 	private Image _bulletImage;
 	private BulletController _controller;
-	
-	public int ChamberIndex => _slotIndex;
+
+    [SerializeField] private BulletView bulletView;
+
+    public int ChamberIndex => _slotIndex;
 	public BulletController Controller { get { return _controller; } set { _controller = value; } }
-	
-	public void UpdateView(BulletObject bulletToDisplay)
+
+    public void UpdateView(BulletObject bulletToDisplay)
 	{
-		_bulletImage = GetComponent<Image>();
-		_bulletToDisplay = bulletToDisplay;
-		
-		_bulletImage.color = _bulletToDisplay != null ? _bulletToDisplay.color : Color.white;
+        //_bulletImage = GetComponent<Image>();
+        //_bulletImage.color = _bulletToDisplay != null ? _bulletToDisplay.color : Color.white;
+        _bulletToDisplay = bulletToDisplay;
 	}
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount == 0)
+        {
+            GameObject dropped = eventData.pointerDrag;
+            BulletDragUI draggedItem = dropped.GetComponent<BulletDragUI>();
+            draggedItem.parentAfterDrag = transform;
+
+            UpdateView(draggedItem.bulletObject);
+
+            if (draggedItem.currentSlot == BulletDragUI.SlotTypes.Inventory)
+            {
+                bulletView.AddBulletToChamber(draggedItem.parentBeforeDragIndex, ChamberIndex);
+            }
+            else if (draggedItem.currentSlot == BulletDragUI.SlotTypes.Chamber)
+            {
+                bulletView.SwapBullets(draggedItem.parentBeforeDragIndex, ChamberIndex);
+            }
+        }
+    }
 }
--- Assets/_Project/Scripts/Inventory/InventorySlotView.cs
+++ Assets/_Project/Scripts/Inventory/InventorySlotView.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class InventorySlotView : MonoBehaviour
+public class InventorySlotView : MonoBehaviour, IDropHandler
 {
 	[SerializeField] private int _slotIndex;
+    [SerializeField] private GameObject bulletDragPrefab;
+
+    [SerializeField] private BulletView bulletView;
 	
 	private BulletObject _bulletToDisplay;
 	private Image _bulletImage;
@@ -13,12 +17,46 @@ public class InventorySlotView : MonoBehaviour
 	
 	public int InventoryIndex => _slotIndex;
 	public BulletController Controller { get { return _controller; } set { _controller = value; } }
-	
-	public void UpdateView(BulletObject bulletToDisplay)
+
+    public void UpdateView(BulletObject bulletToDisplay)
 	{
-		_bulletImage = GetComponent<Image>();
-		_bulletToDisplay = bulletToDisplay;
-		
-		_bulletImage.color = _bulletToDisplay != null ? _bulletToDisplay.color : Color.white;
+        //_bulletImage = GetComponent<Image>();
+        //_bulletImage.color = _bulletToDisplay != null ? _bulletToDisplay.color : Color.white;
+
+        _bulletToDisplay = bulletToDisplay;
+
+        if (transform.childCount == 0 && bulletToDisplay != null)
+        {
+            GameObject bulletUI = Instantiate(bulletDragPrefab);
+            bulletUI.transform.SetParent(this.transform, false);
+
+            BulletDragUI bulletDrag = bulletUI.GetComponent<BulletDragUI>();
+            bulletDrag.parentBeforeDragIndex = InventoryIndex;
+        }
 	}
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (transform.childCount == 0)
+        {
+            GameObject dropped = eventData.pointerDrag;
+            BulletDragUI draggedItem = dropped.GetComponent<BulletDragUI>();
+
+            //If the previous slot was an Inventory slot, do nothing
+            if (draggedItem.currentSlot == BulletDragUI.SlotTypes.Inventory)
+                return;
+
+            draggedItem.parentAfterDrag = transform;
+            UpdateView(draggedItem.bulletObject);
+
+            if (draggedItem.currentSlot == BulletDragUI.SlotTypes.Chamber)
+            {
+                //Destroy draggedItem bc the BulletView loop should handle creating a new UI element
+                int bulletIndex = draggedItem.parentBeforeDragIndex;
+                Destroy(draggedItem.gameObject);
+
+                bulletView.RemoveBulletFromChamber(bulletIndex);
+            }
+        }
+    }
 }
