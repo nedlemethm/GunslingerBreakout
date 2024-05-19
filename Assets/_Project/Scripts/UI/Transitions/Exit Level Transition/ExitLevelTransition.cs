6b32728491feef25854503edd428d2fa5b3eb9b6
big-jungus <matthewjung1010@gmail.com>
2024-05-18 23:33:06 -0700

    testing exit animation

---
+++ Assets/_Project/Scripts/UI/Transitions/Exit Level Transition/ExitLevelTransition.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitLevelTransition : MonoBehaviour
+{
+    [SerializeField] private CaptureScreen captureScreen;
+    private Coroutine transitionRoutine;
+
+    [Header("Animation Variables")]
+    [SerializeField] private float animTime;
+    [SerializeField] private GameObject rightDoor;
+    [SerializeField] private AnimationCurve rightDoorCurve;
+
+    void Start()
+    {
+        // Move the captureCamera to the player's position, and take a "screenshot" of their current view
+        captureScreen.CaptureView(GameObject.FindGameObjectWithTag("MainCamera").transform.position);
+        StartCoroutine(ExitRoutine());
+    }
+
+    private IEnumerator ExitRoutine()
+    {
+        yield return StartCoroutine(Animation());
+        // Call the next scene load here
+    }
+
+    private IEnumerator Animation()
+    {
+        float currentTime = 0f;
+        while (currentTime < animTime)
+        {
+            yield return null;
+            currentTime += Time.deltaTime;
+
+            rightDoor.transform.localEulerAngles = new Vector3(rightDoor.transform.localEulerAngles.x, rightDoorCurve.Evaluate(currentTime / animTime), rightDoor.transform.localEulerAngles.z);
+        }
+    }
+}
